06d5752b2be2c8329328161ee6917baef2d36575
terpsihora96 <dzedaj996@gmail.com>
2019-08-23 20:53:58 +0200

    Model - minor changes.

---
+++ server/models/choice-model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ChoiceSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        films: { type: [String] },
+    }
+)
+
+module.exports = mongoose.model('choices', Choice)
