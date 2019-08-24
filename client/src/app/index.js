c3489227c0a1d4c0ebe2678cc4c9635bdf44b5e9
terpsihora96 <dzedaj996@gmail.com>
2019-08-24 16:58:39 +0200

    UI changed.

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -3,20 +3,27 @@ import { User, Films, Add, ListUsers } from '../components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
-
+import Typography from '@material-ui/core/Typography';
+import './App.css'
 
 function App() {
   return (
-    // Fragment needed for being able to use multiple JSX
-    // Setting up the routes
-
     <Fragment>
-      <Films></Films>
-      <User></User>
-      <Fab size="small" color="default" aria-label="add" href="\user" >
-          <AddIcon />
-      </Fab>
-    
+      <Typography variant="h1" component="h2" >
+        Frielms
+      </Typography>
+      <div className="main">
+        <Films></Films>
+        <div>
+          <User></User>
+          <div className="add">
+            <Fab size="small" color="default" aria-label="add" href="\user" >
+              <AddIcon />
+            </Fab>
+          </div>
+        </div> 
+      </div>  
+      
       <Router>
         <Switch>
           <Route exact path='/user' component={ Add } />
