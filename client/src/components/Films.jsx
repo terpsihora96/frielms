55806d084868959b7fc38e480359be7f9a7d078e
terpsihora96 <dzedaj996@gmail.com>
2019-08-24 17:11:00 +0200

    Films styled.

--- client/src/components/Films.jsx
+++ client/src/components/Films.jsx
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import api from '../api'
+import './Films.css'
 
 class Films extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+          data: [],
+          isLoading: false,
+      }
+  }
+
+  // Get list of films
+  // TODO
+  componentDidMount = async () => {
+    this.setState({ isLoading: true })
+    await api.getFilmTitles()
+    .then(res => this.setState({ data: res.data.data }))
+  }
 
   render() {
     return (
-      <div>
+      <div className="films">
+        {this.data}
         <FormControl >
           <InputLabel shrink htmlFor="select-multiple-native">
             Films
@@ -15,7 +33,6 @@ class Films extends Component {
           <Select
             multiple
             native
-            
           >
           <option >
             Film
