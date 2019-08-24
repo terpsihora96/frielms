dbc7c59ec09b74f771129266e9991429fac567bf
terpsihora96 <dzedaj996@gmail.com>
2019-08-24 11:55:44 +0200

    Add button added.

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
 import { User, Films } from '../components';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 
 function App() {
   return (
     <Fragment>
       <Films></Films>
       <User></User>
+
+      <Fab size="small" color="default" aria-label="add" href="\user" >
+          <AddIcon />
+      </Fab>
+
     </Fragment>    
   )
 }
